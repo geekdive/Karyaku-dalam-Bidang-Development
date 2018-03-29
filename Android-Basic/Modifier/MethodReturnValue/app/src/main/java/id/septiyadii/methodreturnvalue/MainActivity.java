c5cf0c115f1d7edcb59d7d4a678d2a77a296ca7b
Septiyadi <septiyadi@mhs.pelitabangsa.ac.id>
2018-03-30 02:47:08 +0700

    Akses Method & Return Value
    30 Maret 2018 - Mengakses Mehtod biasa dengan Method Return Value

---
+++ Android-Basic/Modifier/MethodReturnValue/app/src/main/java/id/septiyadii/methodreturnvalue/MainActivity.java
@@ -0,0 +1,43 @@
+package id.septiyadii.methodreturnvalue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAksesMethod1 = (Button)findViewById(R.id.btnAksesMethodBiasa);
+        Button btnAksesMethod2 = (Button)findViewById(R.id.btnAksesMethodReturnValue);
+
+        btnAksesMethod1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MethodBiasa();
+            }
+        });
+
+        btnAksesMethod2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MethodReturnValue("Mengakses Method dengan Return Value");
+            }
+        });
+    }
+
+    //TODO 1: Method Biasa tanpa Value
+    public void MethodBiasa(){
+        Toast.makeText(this, "Mengakses Method Biasa dengan Button", Toast.LENGTH_SHORT).show();
+    }
+
+    //TODO 2: Method dengan Return Value
+    public void MethodReturnValue(String isipesan){
+        Toast.makeText(this, isipesan, Toast.LENGTH_SHORT).show();
+    }
+}
