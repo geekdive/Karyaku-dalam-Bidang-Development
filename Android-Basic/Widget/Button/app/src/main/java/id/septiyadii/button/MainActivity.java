6a763b8c298aa7b61968bb70e6b229165f3786fd
Septiyadi <septiyadi@mhs.pelitabangsa.ac.id>
2018-03-30 01:42:23 +0700

    Widget Button & onClickListener  #2 Update
    30 Maret 2018 - Menggunakan Widget Button dan onClickListener

--- Android-Basic/Widget/Button/app/src/main/java/id/septiyadii/button/MainActivity.java
+++ Android-Basic/Widget/Button/app/src/main/java/id/septiyadii/button/MainActivity.java
@@ -2,12 +2,25 @@ package id.septiyadii.button;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnToast = (Button)findViewById(R.id.btnButton);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is Toast!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
